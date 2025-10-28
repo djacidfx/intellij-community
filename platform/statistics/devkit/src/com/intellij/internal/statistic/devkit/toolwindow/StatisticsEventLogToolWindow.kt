7397f2edf75b65f29d01504baf4fc8f0717a7a92
Andrew Kashukov <andrew.kashukov@jetbrains.com>
2025-10-28 12:09:49 +0100

    [dotnet] [fus] RSRP-500513 (WIP) implement basic backend passthrough
    Space-RevId: bff1cd88a0ff501cfd0cb3d2fe4037157cc3ac8c
    
    GitOrigin-RevId: 4a45750e39421116110c09a5a233451597310f11

--- platform/statistics/devkit/src/com/intellij/internal/statistic/devkit/toolwindow/StatisticsEventLogToolWindow.kt
+++ platform/statistics/devkit/src/com/intellij/internal/statistic/devkit/toolwindow/StatisticsEventLogToolWindow.kt
@@ -34,6 +34,9 @@ import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.actionSystem.ToggleAction
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.service
+import com.intellij.openapi.extensions.ExtensionPointListener
+import com.intellij.openapi.extensions.ExtensionPointName
+import com.intellij.openapi.extensions.PluginDescriptor
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.Task
@@ -42,7 +45,9 @@ import com.intellij.openapi.ui.SimpleToolWindowPanel
 import com.intellij.openapi.util.Disposer
 import com.intellij.ui.FilterComponent
 import com.intellij.ui.JBColor
+import com.intellij.util.application
 import com.jetbrains.fus.reporting.model.lion3.LogEvent
+import org.jetbrains.annotations.ApiStatus
 import java.awt.BorderLayout
 import java.awt.FlowLayout
 import javax.swing.BorderFactory
@@ -87,6 +92,23 @@ internal class StatisticsEventLogToolWindow(project: Project, private val record
 
     Disposer.register(this, consoleLog)
     service<EventLogListenersManager>().subscribe(eventLogListener, recorderId)
+
+    if (application.extensionArea.hasExtensionPoint(StatisticsEventLogToolWindowEPLogProvider.EP_NAME)) {
+      fun StatisticsEventLogToolWindowEPLogProvider.subscribe() = subscribe(recorderId, consoleLog::addLogLine)
+
+      StatisticsEventLogToolWindowEPLogProvider.EP_NAME.extensionList.forEach {
+        it.subscribe()
+      }
+      StatisticsEventLogToolWindowEPLogProvider.EP_NAME.addExtensionPointListener(object : ExtensionPointListener<StatisticsEventLogToolWindowEPLogProvider> {
+        override fun extensionAdded(extension: StatisticsEventLogToolWindowEPLogProvider, pluginDescriptor: PluginDescriptor) {
+          extension.subscribe()
+        }
+
+        override fun extensionRemoved(extension: StatisticsEventLogToolWindowEPLogProvider, pluginDescriptor: PluginDescriptor) {
+          extension.unsubscribe(recorderId)
+        }
+      })
+    }
   }
 
   private fun createActionToolbar(): JComponent {
@@ -158,4 +180,16 @@ internal class StatisticsMultilineLogToggleAction(private val consoleLog: Statis
   override fun getActionUpdateThread(): ActionUpdateThread {
     return ActionUpdateThread.BGT
   }
+}
+
+@ApiStatus.Internal
+interface StatisticsEventLogToolWindowEPLogProvider {
+  companion object {
+    @JvmField
+    val EP_NAME: ExtensionPointName<StatisticsEventLogToolWindowEPLogProvider> =
+      ExtensionPointName.create<StatisticsEventLogToolWindowEPLogProvider>("com.intellij.internal.statistic.devkit.toolwindow.eventLogToolWindowEPLogProvider")
+  }
+
+  fun subscribe(recorderId: String, function: (String) -> Unit)
+  fun unsubscribe(recorderId: String)
 }
