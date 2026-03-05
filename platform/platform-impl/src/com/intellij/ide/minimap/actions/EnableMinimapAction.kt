4d31d2fccd7fa960afbde2f90c97466a78b1dcac
Bogdan Kirilenko <bogdan.kirilenko@jetbrains.com>
2026-03-05 20:02:06 +0100

    [platform] PY-88182 added disable minimap action under right-click menu and added EnableMinimap action
    (cherry picked from commit eb1baabd13202c92d089345f13e67aacc3b4f539)
    
    IJ-MR-194257
    
    GitOrigin-RevId: 5433d7f6d9d91e72d0d87b41750213e3e1606bb5

---
+++ platform/platform-impl/src/com/intellij/ide/minimap/actions/EnableMinimapAction.kt
@@ -0,0 +1,24 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.ide.minimap.actions
+
+import com.intellij.ide.minimap.settings.MinimapSettings
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class EnableMinimapAction : AnAction() {
+  override fun isDumbAware(): Boolean = true
+
+  override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
+
+  override fun update(e: AnActionEvent) {
+    e.presentation.isEnabled = !MinimapSettings.getInstance().state.enabled
+  }
+
+  override fun actionPerformed(e: AnActionEvent) {
+    val settings = MinimapSettings.getInstance()
+    if (settings.state.enabled) return
+    settings.state.enabled = true
+    settings.settingsChangeCallback.notify(MinimapSettings.SettingsChangeType.WithUiRebuild)
+  }
+}
