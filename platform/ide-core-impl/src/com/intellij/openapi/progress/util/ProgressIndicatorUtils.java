3edb56b8433176ff7c1e14b36e75f0769452024e
Max Medvedev <maxim.medvedev@jetbrains.com>
2026-03-10 19:04:02 +0100

    KTIJ-37556 extract ProgressIndicatorUtils for use in FileManagerImpl
    GitOrigin-RevId: 2bede9cd9ff9be09d4644f2ffd98e8631d927580

--- platform/ide-core-impl/src/com/intellij/openapi/progress/util/ProgressIndicatorUtils.java
+++ platform/ide-core-impl/src/com/intellij/openapi/progress/util/ProgressIndicatorUtils.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.progress.util;
 
 import com.intellij.codeWithMe.ClientId;
@@ -13,12 +13,10 @@ import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.application.ex.ApplicationEx;
 import com.intellij.openapi.application.ex.ApplicationManagerEx;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.progress.Cancellation;
 import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressIndicatorProvider;
 import com.intellij.openapi.progress.ProgressManager;
-import com.intellij.openapi.progress.impl.CoreProgressManager;
 import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.ThrowableComputable;
@@ -426,44 +424,16 @@ public final class ProgressIndicatorUtils {
   }
 
   public static void awaitWithCheckCanceled(@NotNull Lock lock) {
-    awaitWithCheckCanceled(() -> lock.tryLock(ConcurrencyUtil.DEFAULT_TIMEOUT_MS, MILLISECONDS));
+    ProgressIndicatorUtilBase.awaitWithCheckCanceled(lock);
   }
 
   public static void awaitWithCheckCanceled(@NotNull ThrowableComputable<Boolean, ? extends Exception> waiter) {
-    ProgressIndicator indicator = ProgressManager.getInstance().getProgressIndicator();
-    boolean success = false;
-    while (!success) {
-      checkCancelledEvenWithPCEDisabled(indicator);
-      try {
-        success = waiter.compute();
-      }
-      catch (ProcessCanceledException pce) {
-        throw pce;
-      }
-      catch (Exception e) {
-        //noinspection InstanceofCatchParameter
-        if (!(e instanceof InterruptedException)) {
-          LOG.warn(e);
-        }
-        throw new ProcessCanceledException(e);
-      }
-    }
+    ProgressIndicatorUtilBase.awaitWithCheckCanceled(waiter);
   }
 
   /** Use when a deadlock is possible otherwise. */
   public static void checkCancelledEvenWithPCEDisabled(@Nullable ProgressIndicator indicator) {
-    boolean isNonCancelable = Cancellation.isInNonCancelableSection();
-    if (isNonCancelable || indicator == null) {
-      ((CoreProgressManager)ProgressManager.getInstance()).runCheckCanceledHooks(indicator);
-    }
-    if (isNonCancelable) return;
-    Cancellation.ensureActive();
-    if (indicator == null) return;
-    indicator.checkCanceled();              // check for cancellation as usual and run the hooks
-    if (indicator.isCanceled()) {           // if a just-canceled indicator or PCE is disabled
-      indicator.checkCanceled();            // ... let the just-canceled indicator provide a customized PCE
-      throw new ProcessCanceledException(); // ... otherwise PCE is disabled so throw it manually
-    }
+    ProgressIndicatorUtilBase.checkCancelledEvenWithPCEDisabled(indicator);
   }
 
   public static void awaitWithCheckCanceled(@NotNull Semaphore semaphore, @Nullable ProgressIndicator indicator) {
