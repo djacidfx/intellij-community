744ed67685096bf66c2ae9c763640878d7f47620
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2026-03-24 14:23:17 +0100

    [java] Add annotation roots to workspace file index
    The platform's WorkspaceFileIndex only registers COMPILED and SOURCES roots for libraries. The ANNOTATIONS root type (registered by the Java plugin) was not covered, so files under those roots were invisible to the indexing infrastructure. This commit adds the workspace file index for annotations roots in both SDKs and libraries. Besides that it adjusts some existings tests to use test.* annotation names instead of org.jetbrains.annotations.* to avoid collisions with real annotations on the classpath. A side-effect of this change is that usages will now be found in `annotations.xml` #IDEA-387817 Fixed
    
    GitOrigin-RevId: 42c6114d82feb21b444f43d43b8719d09943af0a

--- java/java-tests/testSrc/com/intellij/java/index/ExternalAnnotationsIndexTest.java
+++ java/java-tests/testSrc/com/intellij/java/index/ExternalAnnotationsIndexTest.java
@@ -1,13 +1,70 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.java.index;
 
+import com.intellij.openapi.application.WriteAction;
+import com.intellij.openapi.projectRoots.ProjectJdkTable;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.openapi.roots.AnnotationOrderRootType;
+import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.libraries.Library;
+import com.intellij.openapi.roots.libraries.LibraryTable;
+import com.intellij.openapi.util.io.NioFiles;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.ThrowableConsumer;
 import com.intellij.psi.impl.java.stubs.index.ExternalAnnotationsIndex;
 import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.testFramework.IndexingTestUtil;
+import com.intellij.testFramework.PsiTestUtil;
 import com.intellij.testFramework.fixtures.JavaCodeInsightFixtureTestCase;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 public class ExternalAnnotationsIndexTest extends JavaCodeInsightFixtureTestCase {
+  private static void withExternalAnnotations(String xmlRelativePath, String xmlContent,
+                                       ThrowableConsumer<? super VirtualFile, ? extends Exception> test) throws Exception {
+    Path externalAnnotationsDir = Files.createTempDirectory("extAnnotations");
+    try {
+      Path xmlPath = externalAnnotationsDir.resolve(xmlRelativePath);
+      Files.createDirectories(xmlPath.getParent());
+      Files.writeString(xmlPath, xmlContent);
+      VirtualFile root = LocalFileSystem.getInstance().refreshAndFindFileByNioFile(externalAnnotationsDir);
+      assertNotNull(root);
+      test.consume(root);
+    }
+    finally {
+      NioFiles.deleteRecursively(externalAnnotationsDir);
+    }
+  }
+
+  private void addLibraryAnnotationRoot(VirtualFile root) {
+    WriteAction.run(() -> {
+      var model = ModuleRootManager.getInstance(myFixture.getModule()).getModifiableModel();
+      LibraryTable libTable = model.getModuleLibraryTable();
+      Library library = libTable.createLibrary("testLib");
+      Library.ModifiableModel libModel = library.getModifiableModel();
+      libModel.addRoot(root, AnnotationOrderRootType.getInstance());
+      libModel.commit();
+      model.commit();
+    });
+    IndexingTestUtil.waitUntilIndexesAreReady(getProject());
+  }
+
+  private void addSdkAnnotationRoot(VirtualFile root) {
+    Sdk originalSdk = ModuleRootManager.getInstance(myFixture.getModule()).getSdk();
+    assertNotNull(originalSdk);
+    Sdk sdkWithAnnotations = PsiTestUtil.addRootsToJdk(originalSdk, AnnotationOrderRootType.getInstance(), root);
+    WriteAction.run(() -> {
+      ProjectJdkTable.getInstance().addJdk(sdkWithAnnotations, getTestRootDisposable());
+      var model = ModuleRootManager.getInstance(myFixture.getModule()).getModifiableModel();
+      model.setSdk(sdkWithAnnotations);
+      model.commit();
+    });
+    IndexingTestUtil.waitUntilIndexesAreReady(getProject());
+  }
+
   public void testSimple() {
     myFixture.addFileToProject("annotations.xml", """
       <root>
@@ -35,7 +92,7 @@ public class ExternalAnnotationsIndexTest extends JavaCodeInsightFixtureTestCase
     myFixture.addFileToProject("pkg1/annotations.xml", """
       <root>
         <item name="com.example.Foo">
-          <annotation name="org.jetbrains.annotations.NotNull"/>
+          <annotation name="test.NotNull"/>
         </item>
       </root>
       """);
@@ -43,8 +100,8 @@ public class ExternalAnnotationsIndexTest extends JavaCodeInsightFixtureTestCase
     myFixture.addFileToProject("pkg2/annotations.xml", """
       <root>
         <item name="com.example.Bar">
-          <annotation name="org.jetbrains.annotations.Nullable"/>
-          <annotation name="org.jetbrains.annotations.Contract">
+          <annotation name="test.Nullable"/>
+          <annotation name="test.Contract">
             <val name="value" val="&quot;null -&gt; null&quot;"/>
           </annotation>
         </item>
@@ -52,9 +109,9 @@ public class ExternalAnnotationsIndexTest extends JavaCodeInsightFixtureTestCase
       """);
 
     GlobalSearchScope scope = GlobalSearchScope.allScope(getProject());
-    assertSameElements(ExternalAnnotationsIndex.getItemsByAnnotation("org.jetbrains.annotations.NotNull", scope), "com.example.Foo");
-    assertSameElements(ExternalAnnotationsIndex.getItemsByAnnotation("org.jetbrains.annotations.Nullable", scope), "com.example.Bar");
-    assertSameElements(ExternalAnnotationsIndex.getItemsByAnnotation("org.jetbrains.annotations.Contract", scope), "com.example.Bar");
+    assertSameElements(ExternalAnnotationsIndex.getItemsByAnnotation("test.NotNull", scope), "com.example.Foo");
+    assertSameElements(ExternalAnnotationsIndex.getItemsByAnnotation("test.Nullable", scope), "com.example.Bar");
+    assertSameElements(ExternalAnnotationsIndex.getItemsByAnnotation("test.Contract", scope), "com.example.Bar");
   }
 
   public void testNonAnnotationsXmlIgnored() {
@@ -79,7 +136,7 @@ public class ExternalAnnotationsIndexTest extends JavaCodeInsightFixtureTestCase
     myFixture.addFileToProject("annotations.xml", """
       <root>
         <item name="com.example.Foo java.lang.String bar()">
-          <annotation name="org.jetbrains.annotations.Contract">
+          <annotation name="test.Contract">
             <val name="value" val="&quot;!null&quot;"/>
             <val name="pure" val="true"/>
           </annotation>
@@ -88,7 +145,7 @@ public class ExternalAnnotationsIndexTest extends JavaCodeInsightFixtureTestCase
       """);
 
     List<String> items =
-      ExternalAnnotationsIndex.getItemsByAnnotation("org.jetbrains.annotations.Contract", GlobalSearchScope.allScope(getProject()));
+      ExternalAnnotationsIndex.getItemsByAnnotation("test.Contract", GlobalSearchScope.allScope(getProject()));
     assertSameElements(items, "com.example.Foo java.lang.String bar()");
   }
 
@@ -96,17 +153,17 @@ public class ExternalAnnotationsIndexTest extends JavaCodeInsightFixtureTestCase
     myFixture.addFileToProject("annotations.xml", """
       <root>
         <item name="com.example.Foo java.lang.String[] bar()">
-          <annotation name="org.jetbrains.annotations.NotNull"/>
-          <annotation name="org.jetbrains.annotations.NotNull" typePath="/[]"/>
+          <annotation name="test.NotNull"/>
+          <annotation name="test.NotNull" typePath="/[]"/>
         </item>
         <item name="com.example.Bar java.util.List baz()">
-          <annotation name="org.jetbrains.annotations.NotNull" typePath="/0;"/>
+          <annotation name="test.NotNull" typePath="/0;"/>
         </item>
       </root>
       """);
 
     GlobalSearchScope scope = GlobalSearchScope.allScope(getProject());
-    List<String> items = ExternalAnnotationsIndex.getItemsByAnnotation("org.jetbrains.annotations.NotNull", scope);
+    List<String> items = ExternalAnnotationsIndex.getItemsByAnnotation("test.NotNull", scope);
     // First item has an element-level @NotNull, so it should appear.
     // Second item only has a type-level @NotNull (typePath), so it should NOT appear.
     assertSameElements(items, "com.example.Foo java.lang.String[] bar()");
@@ -142,5 +199,74 @@ public class ExternalAnnotationsIndexTest extends JavaCodeInsightFixtureTestCase
 
     List<String> items = ExternalAnnotationsIndex.getItemsByAnnotation("test.Ann", GlobalSearchScope.allScope(getProject()));
     assertSameElements(items, "pkg1.A", "pkg2.B");
+  }
+
+  public void testAnnotationRootIsIndexed() throws Exception {
+    withExternalAnnotations("com/example/annotations.xml", """
+      <root>
+        <item name="com.example.Foo">
+          <annotation name="test.LibAnn"/>
+        </item>
+        <item name="com.example.Foo void bar()">
+          <annotation name="test.LibAnn"/>
+        </item>
+      </root>
+      """, annRoot -> {
+      addLibraryAnnotationRoot(annRoot);
+
+      List<String> items = ExternalAnnotationsIndex.getItemsByAnnotation("test.LibAnn", GlobalSearchScope.allScope(getProject()));
+      assertSameElements(items, "com.example.Foo", "com.example.Foo void bar()");
+    });
+  }
+
+  public void testAnnotationRootIndexUpdatesOnXmlChange() throws Exception {
+    withExternalAnnotations("com/example/annotations.xml", """
+      <root>
+        <item name="com.example.Foo">
+          <annotation name="test.Upd"/>
+        </item>
+      </root>
+      """, annRoot -> {
+      addLibraryAnnotationRoot(annRoot);
+
+      GlobalSearchScope scope = GlobalSearchScope.allScope(getProject());
+      assertSameElements(ExternalAnnotationsIndex.getItemsByAnnotation("test.Upd", scope), "com.example.Foo");
+
+      // Modify the XML: add a new item, remove the old one
+      VirtualFile xmlFile = annRoot.findFileByRelativePath("com/example/annotations.xml");
+      assertNotNull(xmlFile);
+      WriteAction.run(() -> xmlFile.setBinaryContent("""
+        <root>
+          <item name="com.example.Bar">
+            <annotation name="test.Upd"/>
+          </item>
+          <item name="com.example.Baz">
+            <annotation name="test.Upd2"/>
+          </item>
+        </root>
+        """.getBytes(java.nio.charset.StandardCharsets.UTF_8)));
+      IndexingTestUtil.waitUntilIndexesAreReady(getProject());
+
+      assertSameElements(ExternalAnnotationsIndex.getItemsByAnnotation("test.Upd", scope), "com.example.Bar");
+      assertSameElements(ExternalAnnotationsIndex.getItemsByAnnotation("test.Upd2", scope), "com.example.Baz");
+    });
+  }
+
+  public void testSdkAnnotationRootIsIndexed() throws Exception {
+    withExternalAnnotations("annotations.xml", """
+      <root>
+        <item name="java.lang.String">
+          <annotation name="test.SdkAnn"/>
+        </item>
+        <item name="java.lang.String int length()">
+          <annotation name="test.SdkAnn"/>
+        </item>
+      </root>
+      """, annRoot -> {
+      addSdkAnnotationRoot(annRoot);
+
+      List<String> items = ExternalAnnotationsIndex.getItemsByAnnotation("test.SdkAnn", GlobalSearchScope.allScope(getProject()));
+      assertSameElements(items, "java.lang.String", "java.lang.String int length()");
+    });
   }
 }
