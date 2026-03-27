6ca6e89321c201cb364b1ef2c85c665325e0b844
Bogdan Kirilenko <bogdan.kirilenko@jetbrains.com>
2026-03-27 18:37:17 +0100

    [platform] PY-88182 added minimap to EditorTabsEntryPoint
    (cherry picked from commit 1b05f8600d5574caa18c3a41351d91f91171c843)
    
    IJ-MR-194257
    
    GitOrigin-RevId: 4f014a5e065d08f28aa8fabdcfaa5f5a0dbbcbf3

--- platform/platform-impl/src/com/intellij/ide/minimap/actions/ToggleMinimapAction.kt
+++ platform/platform-impl/src/com/intellij/ide/minimap/actions/ToggleMinimapAction.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.minimap.actions
 
+import com.intellij.ide.minimap.MinimapRegistry
 import com.intellij.ide.minimap.MinimapUsageCollector
 import com.intellij.ide.minimap.settings.MinimapSettings
 import com.intellij.openapi.actionSystem.ActionUpdateThread
@@ -10,6 +11,11 @@ import com.intellij.openapi.actionSystem.remoting.ActionRemoteBehaviorSpecificat
 import com.intellij.openapi.project.DumbAwareToggleAction
 
 class ToggleMinimapAction : DumbAwareToggleAction(), RightAlignedToolbarAction, ActionRemoteBehaviorSpecification.Frontend {
+  override fun update(e: AnActionEvent) {
+    super.update(e)
+    e.presentation.isEnabledAndVisible = MinimapRegistry.isEnabled()
+  }
+
   override fun isSelected(e: AnActionEvent): Boolean = MinimapSettings.getInstance().state.enabled
   override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
   override fun setSelected(e: AnActionEvent, state: Boolean) {
