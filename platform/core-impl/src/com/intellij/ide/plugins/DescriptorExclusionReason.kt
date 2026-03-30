3775f3a9aa46d75e2dfe7870807014851d5d4cc2
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2026-03-30 18:54:12 +0200

    IJPL-236591 fix api status annotations
    for some reason ApiCheckTest thinks that interfaces are in the API while there is `@file:ApiStatus.Internal` annotation...
    
    GitOrigin-RevId: b3f554da243dca99758ee43ab48f9aaf6206503c

--- platform/core-impl/src/com/intellij/ide/plugins/DescriptorExclusionReason.kt
+++ platform/core-impl/src/com/intellij/ide/plugins/DescriptorExclusionReason.kt
@@ -1,24 +1,26 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-@file:ApiStatus.Internal
-
 package com.intellij.ide.plugins
 
 import org.jetbrains.annotations.ApiStatus
 
 
+@ApiStatus.Internal
 sealed interface DescriptorExclusionReason {
   val descriptor: IdeaPluginDescriptorImpl
 }
 
+@ApiStatus.Internal
 sealed interface ChainedExclusion {
   val precedingExcludedDescriptor: IdeaPluginDescriptorImpl
 }
 
+@ApiStatus.Internal
 class DependencyIsNotResolved(
   override val descriptor: IdeaPluginDescriptorImpl,
   val dependency: PluginDependencyAnalysis.DependencyRef,
 ) : DescriptorExclusionReason
 
+@ApiStatus.Internal
 class DependencyIsExcluded(
   override val descriptor: IdeaPluginDescriptorImpl,
   val dependencyModule: PluginModuleDescriptor,
@@ -26,27 +28,32 @@ class DependencyIsExcluded(
   override val precedingExcludedDescriptor: IdeaPluginDescriptorImpl get() = dependencyModule
 }
 
+@ApiStatus.Internal
 class DependencyIsNotVisible(
   override val descriptor: IdeaPluginDescriptorImpl,
   val dependencyModule: PluginModuleDescriptor,
   val visibilityViolationLogMessage: String,
 ) : DescriptorExclusionReason
 
+@ApiStatus.Internal
 class IncompatibleWithAnotherModule(
   override val descriptor: IdeaPluginDescriptorImpl,
   val preferredIncompatibleModule: PluginModuleDescriptor,
 ) : DescriptorExclusionReason
 
+@ApiStatus.Internal
 class PartOfDependencyCycle(
   override val descriptor: IdeaPluginDescriptorImpl,
   val dependencyCycle: DependencyCycleInfo<IdeaPluginDescriptorImpl>,
 ) : DescriptorExclusionReason
 
+@ApiStatus.Internal
 class PartOfRuntimeModuleGroupDependencyCycle(
   override val descriptor: IdeaPluginDescriptorImpl,
   val dependencyCycle: DependencyCycleInfo<RuntimeModuleGroup>,
 ) : DescriptorExclusionReason
 
+@ApiStatus.Internal
 class DependsParentIsExcluded(
   override val descriptor: DependsSubDescriptor,
 ) : DescriptorExclusionReason, ChainedExclusion {
@@ -54,6 +61,7 @@ class DependsParentIsExcluded(
     get() = descriptor.parent
 }
 
+@ApiStatus.Internal
 class ContentModuleParentIsExcluded(
   override val descriptor: ContentModuleDescriptor,
 ) : DescriptorExclusionReason, ChainedExclusion {
@@ -61,6 +69,7 @@ class ContentModuleParentIsExcluded(
     get() = descriptor.parent
 }
 
+@ApiStatus.Internal
 class RequiredContentModuleIsExcluded(
   override val descriptor: PluginMainDescriptor,
   val excludedContentModule: ContentModuleDescriptor,
@@ -69,16 +78,19 @@ class RequiredContentModuleIsExcluded(
     get() = excludedContentModule
 }
 
+@ApiStatus.Internal
 class PackagePrefixConflictWithAnotherModule(
   override val descriptor: PluginModuleDescriptor,
   val preferredConflictingModule: PluginModuleDescriptor,
 ) : DescriptorExclusionReason
 
+@ApiStatus.Internal
 class ExcludedByEnvironmentConfiguration(
   override val descriptor: PluginModuleDescriptor,
   val reason: EnvironmentDependentModuleUnavailabilityReason,
 ) : DescriptorExclusionReason
 
+@ApiStatus.Internal
 class ProductRulesImposedExclusion(
   override val descriptor: PluginModuleDescriptor,
   val productReason: ProductRulesImposedExclusionReason,
@@ -86,9 +98,11 @@ class ProductRulesImposedExclusion(
   interface ProductRulesImposedExclusionReason
 }
 
+@ApiStatus.Internal
 fun DescriptorExclusionReason.getPrecedingLinkInExclusionChain(): IdeaPluginDescriptorImpl? =
   (this as? ChainedExclusion)?.precedingExcludedDescriptor
 
+@ApiStatus.Internal
 fun IdeaPluginDescriptorImpl.sequenceDescriptorExclusionChain(
   getExclusionReason: (IdeaPluginDescriptorImpl) -> DescriptorExclusionReason?,
 ): Sequence<IdeaPluginDescriptorImpl> {
@@ -103,6 +117,7 @@ fun IdeaPluginDescriptorImpl.sequenceDescriptorExclusionChain(
   }
 }
 
+@ApiStatus.Internal
 class DependencyCycleInfo<N>(
   val nodesWithDependenciesOnCycle: Map<N, List<N>>
 )
