5aa227ade23a4042bc8ea11279fbcb41b2af5d73
Marcin Mikosik <marcin.mikosik@jetbrains.com>
2026-03-31 15:47:59 +0200

    IDEA-261477 [lombok] Refactor ExtensionMethodsHelper for completion support
    GitOrigin-RevId: 370469baced2ff62922ec7ca46116358293012fd

--- plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/processor/method/ExtensionMethodsHelper.java
+++ plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/processor/method/ExtensionMethodsHelper.java
@@ -28,7 +28,6 @@ import com.intellij.psi.util.MethodSignatureBackedByPsiMethod;
 import com.intellij.psi.util.PsiModificationTracker;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.util.PsiTypesUtil;
-import com.intellij.util.SmartList;
 import com.intellij.util.containers.ContainerUtil;
 import de.plushnikov.intellij.plugin.LombokClassNames;
 import de.plushnikov.intellij.plugin.psi.LombokExtensionMethod;
@@ -36,7 +35,7 @@ import de.plushnikov.intellij.plugin.psi.LombokLightParameter;
 import de.plushnikov.intellij.plugin.util.PsiAnnotationSearchUtil;
 import de.plushnikov.intellij.plugin.util.PsiAnnotationUtil;
 import de.plushnikov.intellij.plugin.util.PsiClassUtil;
-import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.NotNullByDefault;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
@@ -49,83 +48,108 @@ import java.util.Set;
 import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
+@NotNullByDefault
 public final class ExtensionMethodsHelper {
-
   private static final Logger LOG = Logger.getInstance(ExtensionMethodsHelper.class);
 
-  public static List<PsiExtensionMethod> getExtensionMethods(final @NotNull PsiClass targetClass,
-                                                             final @NotNull String nameHint,
-                                                             final @NotNull PsiElement place) {
-    if (!(place instanceof PsiMethodCallExpression)) {
+  public static List<PsiExtensionMethod> getExtensionMethods(PsiClass targetClass, String nameHint, PsiElement place) {
+    if (!(place instanceof PsiMethodCallExpression methodCallExpression)) {
       return Collections.emptyList();
     }
-    PsiReferenceExpression methodExpression = ((PsiMethodCallExpression)place).getMethodExpression();
+    PsiReferenceExpression methodExpression = methodCallExpression.getMethodExpression();
     PsiExpression qualifierExpression = methodExpression.getQualifierExpression();
     if (qualifierExpression == null ||
         !nameHint.equals(methodExpression.getReferenceName()) ||
         qualifierExpression instanceof PsiReferenceExpression && ((PsiReferenceExpression)qualifierExpression).resolve() instanceof PsiClass) {
       return Collections.emptyList();
     }
-    List<PsiExtensionMethod> result = new SmartList<>();
 
+    return collectExtensionMethods(targetClass, place, (providerClass, extensionMethodImpl) -> createExtensionMethod(
+      providerClass, extensionMethodImpl, targetClass, methodCallExpression));
+  }
+
+  private static List<PsiExtensionMethod> collectExtensionMethods(PsiClass targetClass,
+                                                                  PsiElement place,
+                                                                  BiFunction<PsiClass, PsiMethod, @Nullable PsiExtensionMethod> extensionMethodFactory) {
+    List<PsiExtensionMethod> result = new ArrayList<>();
     @Nullable PsiClass context = PsiTreeUtil.getContextOfType(place, PsiClass.class);
     while (context != null) {
-      final @Nullable PsiAnnotation annotation = PsiAnnotationSearchUtil.findAnnotation(context, LombokClassNames.EXTENSION_METHOD);
-      if (annotation != null) {
-
-        final Set<PsiClass> providers = PsiAnnotationUtil.getAnnotationValues(annotation, PsiAnnotation.DEFAULT_REFERENCED_METHOD_NAME, PsiType.class, List.of()).stream()
-          .filter(PsiClassType.class::isInstance)
-          .map(PsiClassType.class::cast)
-          .map(PsiClassType::resolve)
-          .filter(Objects::nonNull)
-          .collect(Collectors.toSet());
-
-        if (!providers.isEmpty()) {
-          List<PsiExtensionMethod> extensionMethods = collectExtensionMethods(providers, ((PsiMethodCallExpression)place), targetClass);
-          extensionMethods
-            .stream()
-            .map(method -> MethodSignatureBackedByPsiMethod.create(method, PsiSubstitutor.EMPTY))
-            .distinct()
-            .filter(methodSignature -> !targetClass.getVisibleSignatures().contains(methodSignature))
-            .forEach(methodSignature -> result.add((PsiExtensionMethod)methodSignature.getMethod()));
+      Set<PsiClass> providerClasses = findProviderClasses(context);
+      if (!providerClasses.isEmpty()) {
+        List<PsiExtensionMethod> psiMethods = new ArrayList<>();
+        for (PsiClass providerClass : providerClasses) {
+          for (PsiMethod extensionMethodImpl : findExtensionMethodImplCandidatesCached(providerClass)) {
+            ContainerUtil.addIfNotNull(psiMethods, extensionMethodFactory.apply(providerClass, extensionMethodImpl));
+          }
         }
+        result.addAll(filterOutByInstanceMethodSignatures(psiMethods, targetClass));
       }
       context = PsiTreeUtil.getContextOfType(context, PsiClass.class);
     }
+
     return result;
   }
 
-  private static List<PsiExtensionMethod> collectExtensionMethods(final Set<PsiClass> providers,
-                                                         final PsiMethodCallExpression callExpression,
-                                                         final PsiClass targetClass) {
-    List<PsiExtensionMethod> psiMethods = new ArrayList<>();
-    providers.forEach(providerClass -> providerData(providerClass).forEach(function -> ContainerUtil.addIfNotNull(psiMethods, function.apply(targetClass, callExpression))));
-    return psiMethods;
+  /**
+   * Returns classes providing static extension methods that are enabled within the body of `psiClass`
+   * by reading information from the `@ExtensionMethod` annotation attached to `psiClass`, if present.
+   */
+  private static Set<PsiClass> findProviderClasses(PsiClass psiClass) {
+    @Nullable PsiAnnotation annotation = PsiAnnotationSearchUtil.findAnnotation(psiClass, LombokClassNames.EXTENSION_METHOD);
+    if (annotation == null) return Set.of();
+    return PsiAnnotationUtil.getAnnotationValues(annotation, PsiAnnotation.DEFAULT_REFERENCED_METHOD_NAME, PsiType.class, List.of())
+      .stream()
+      .filter(PsiClassType.class::isInstance)
+      .map(PsiClassType.class::cast)
+      .map(PsiClassType::resolve)
+      .filter(Objects::nonNull)
+      .collect(Collectors.toSet());
   }
 
-  public static List<BiFunction<PsiClass, PsiMethodCallExpression, PsiExtensionMethod>> providerData(final PsiClass providerClass) {
+  /// Returns `methods` filtered out by instance method signatures already present in `targetClass`
+  private static List<PsiExtensionMethod> filterOutByInstanceMethodSignatures(List<PsiExtensionMethod> methods,
+                                                                              PsiClass targetClass) {
+    return methods
+      .stream()
+      .map(method -> MethodSignatureBackedByPsiMethod.create(method, PsiSubstitutor.EMPTY))
+      .distinct()
+      .filter(methodSignature -> !targetClass.getVisibleSignatures().contains(methodSignature))
+      .map(methodSignature -> (PsiExtensionMethod)methodSignature.getMethod())
+      .toList();
+  }
+
+  private static List<PsiMethod> findExtensionMethodImplCandidatesCached(PsiClass providerClass) {
     return CachedValuesManager.getCachedValue(providerClass, () -> CachedValueProvider.Result
-      .create(createProviderCandidates(providerClass), PsiModificationTracker.MODIFICATION_COUNT));
+      .create(findExtensionMethodImplCandidates(providerClass), PsiModificationTracker.MODIFICATION_COUNT));
   }
 
-  private static List<BiFunction<PsiClass, PsiMethodCallExpression, PsiExtensionMethod>> createProviderCandidates(final PsiClass providerClass) {
-    final List<BiFunction<PsiClass, PsiMethodCallExpression, PsiExtensionMethod>> result = new ArrayList<>();
-    for (PsiMethod providerStaticMethod : PsiClassUtil.collectClassStaticMethodsIntern(providerClass)) {
-      if (providerStaticMethod.hasModifierProperty(PsiModifier.PUBLIC)) {
-        PsiParameter @NotNull [] parameters = providerStaticMethod.getParameterList().getParameters();
+  /// Finds potential extension method implementation candidates from the provided class by inspecting its static methods.
+  /// Filters methods that are public, have at least one parameter, and whose first parameter is not a primitive type.
+  ///
+  /// @param providerClass the class to scan for extension method candidates.
+  /// @return a list of static methods from the provider class that are eligible as extension method candidates.
+  private static List<PsiMethod> findExtensionMethodImplCandidates(PsiClass providerClass) {
+    final List<PsiMethod> result = new ArrayList<>();
+    for (PsiMethod staticMethod : PsiClassUtil.collectClassStaticMethodsIntern(providerClass)) {
+      if (staticMethod.hasModifierProperty(PsiModifier.PUBLIC)) {
+        PsiParameter[] parameters = staticMethod.getParameterList().getParameters();
         if (parameters.length > 0 && !(parameters[0].getType() instanceof PsiPrimitiveType)) {
-          result.add((targetClass, call) -> createLightMethodBySignature(providerStaticMethod, targetClass, call));
+          result.add(staticMethod);
         }
       }
     }
     return result;
   }
 
-  private static PsiExtensionMethod createLightMethodBySignature(PsiMethod staticMethod,
-                                                                       PsiClass targetClass,
-                                                                       PsiMethodCallExpression callExpression) {
-    if (!staticMethod.getName().equals(callExpression.getMethodExpression().getReferenceName())) return null;
-    PsiClass providerClass = Objects.requireNonNull(staticMethod.getContainingClass());
+  /// Creates a [PsiExtensionMethod] that represents an extension method binding `extensionMethodImpl` from `providerClass`
+  /// to an instance method of `targetClass`.
+  /// Ensures that the provided static method matches the method invoked by the
+  /// given call expression and resolves its applicability or returns null otherwise.
+  private static @Nullable PsiExtensionMethod createExtensionMethod(PsiClass providerClass,
+                                                          PsiMethod extensionMethodImpl,
+                                                          PsiClass targetClass,
+                                                          PsiMethodCallExpression callExpression) {
+    if (!extensionMethodImpl.getName().equals(callExpression.getMethodExpression().getReferenceName())) return null;
     PsiMethodCallExpression staticMethodCall;
     try {
       StringBuilder args = new StringBuilder(Objects.requireNonNull(callExpression.getMethodExpression().getQualifierExpression()).getText());
@@ -135,8 +159,8 @@ public final class ExtensionMethodsHelper {
       }
       args.append(StringUtil.join(expressions, expression -> expression.getText(), ","));
 
-      staticMethodCall = (PsiMethodCallExpression)JavaPsiFacade.getElementFactory(staticMethod.getProject())
-        .createExpressionFromText(providerClass.getQualifiedName() + "." + staticMethod.getName() + "(" + args + ")", callExpression);
+      staticMethodCall = (PsiMethodCallExpression)JavaPsiFacade.getElementFactory(extensionMethodImpl.getProject())
+        .createExpressionFromText(providerClass.getQualifiedName() + "." + extensionMethodImpl.getName() + "(" + args + ")", callExpression);
     }
     catch (ProcessCanceledException e) {
       throw e;
@@ -149,12 +173,18 @@ public final class ExtensionMethodsHelper {
     JavaResolveResult result = staticMethodCall.resolveMethodGenerics();
     if (!(result instanceof MethodCandidateInfo)) return null;
     PsiMethod method = ((MethodCandidateInfo)result).getElement();
-    if (!method.equals(staticMethod) || !((MethodCandidateInfo)result).isApplicable()) return null;
+    if (!method.equals(extensionMethodImpl) || !((MethodCandidateInfo)result).isApplicable()) return null;
     PsiSubstitutor substitutor = result.getSubstitutor();
 
+    return createLightMethod(extensionMethodImpl, targetClass, substitutor);
+  }
+
+  private static PsiExtensionMethod createLightMethod(final PsiMethod staticMethod,
+                                                      final PsiClass targetClass,
+                                                      final PsiSubstitutor substitutor) {
     final LombokExtensionMethod lightMethod = new LombokExtensionMethod(staticMethod);
     lightMethod.addModifiers(PsiModifier.PUBLIC);
-    PsiParameter @NotNull [] parameters = staticMethod.getParameterList().getParameters();
+    PsiParameter[] parameters = staticMethod.getParameterList().getParameters();
 
     if (targetClass.isInterface()) {
       lightMethod.addModifier(PsiModifier.DEFAULT);
@@ -185,4 +215,5 @@ public final class ExtensionMethodsHelper {
     lightMethod.setContainingClass(targetClass);
     return lightMethod;
   }
+
 }
