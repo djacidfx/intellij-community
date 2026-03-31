a658e5987cd16d31adb48a70039690fb3583e6ec
Roman Golyshev <roman.golyshev@jetbrains.com>
2026-03-31 11:27:55 +0200

    [kotlin] KTIJ-38143 Enable `removeContextSensitiveResolutionQualifiers` shortening option by default via registry key
    Registry key is required for the case if the support has to be
    quicky disabled (for example in the release branch)
    
    ^KTIJ-38143 Fixed
    
    GitOrigin-RevId: 59f17f8e193e56c6e6498dd5ae9275c7adbd5f82

--- plugins/kotlin/base/code-insight/src/org/jetbrains/kotlin/idea/base/codeInsight/ShortenOptionsForIde.kt
+++ plugins/kotlin/base/code-insight/src/org/jetbrains/kotlin/idea/base/codeInsight/ShortenOptionsForIde.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.base.codeInsight
 
+import com.intellij.openapi.util.registry.Registry
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.kotlin.analysis.api.components.ShortenOptions
 
@@ -11,7 +12,7 @@ import org.jetbrains.kotlin.analysis.api.components.ShortenOptions
 data class ShortenOptionsForIde(
     val removeThis: Boolean = false,
     val removeThisLabels: Boolean = false,
-    val removeContextSensitiveResolutionQualifiers: Boolean = false,
+    val removeContextSensitiveResolutionQualifiers: Boolean = Registry.`is`("kotlin.shorten.context.sensitive.resolution.qualifiers.by.default"),
     val removeExplicitCompanionReferences: Boolean = true,
 ) {
     fun toShortenOptions(): ShortenOptions = ShortenOptions(removeThis, removeThisLabels, removeContextSensitiveResolutionQualifiers)
