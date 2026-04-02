2592bc6ac31b7a41e5f416169e36cd84c2b4b827
Nikita Katkov <nikita.katkov@jetbrains.com>
2026-04-02 17:10:47 +0200

    [remote development] IJPL-231068 Adjusted k2 package for test
    GitOrigin-RevId: 55515703cf45f9204374ed4b90c29b42f67f9cd3

--- plugins/devkit/devkit-kotlin-fir-tests/testSrc/org/jetbrains/idea/devkit/k2/inspections/remotedev/SplitModeApiUsageInspectionTest.kt
+++ plugins/devkit/devkit-kotlin-fir-tests/testSrc/org/jetbrains/idea/devkit/k2/inspections/remotedev/SplitModeApiUsageInspectionTest.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.idea.devkit.kotlin.inspections.remotedev
+package org.jetbrains.idea.devkit.k2.inspections.remotedev
 
 import com.intellij.testFramework.PsiTestUtil
 import com.intellij.testFramework.common.timeoutRunBlocking
