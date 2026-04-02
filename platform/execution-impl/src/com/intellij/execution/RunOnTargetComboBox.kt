0f8102a38819b5574de3b9d569177b08669bab6b
Pavel Porvatov <Pavel.Porvatov@jetbrains.com>
2026-04-02 18:42:00 +0200

    IJPL-241858 Support rounded selection in ComboBox-es of Run/Debug Configurations
    - Rounded selection for the RunOn field
    
    GitOrigin-RevId: 638fff77f418b73aa1fae3fa0b55a6af65620458

--- platform/execution-impl/src/com/intellij/execution/RunOnTargetComboBox.kt
+++ platform/execution-impl/src/com/intellij/execution/RunOnTargetComboBox.kt
@@ -13,18 +13,23 @@ import com.intellij.icons.AllIcons
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.ValidationInfo
 import com.intellij.openapi.util.NlsContexts
+import com.intellij.ui.dsl.listCellRenderer.listCellRenderer
 import org.jetbrains.annotations.ApiStatus
+import java.util.function.Supplier
 import javax.swing.Icon
+import javax.swing.ListCellRenderer
 
 
 @ApiStatus.Internal
-sealed class Item(val displayName: @NlsContexts.Label String, open val icon: Icon?)
+sealed class Item(val displayName: @NlsContexts.Label String, open val icon: Icon?, val separator: @NlsContexts.Separator String? = null)
 
 @ApiStatus.Internal
-class Separator(displayName: @NlsContexts.Label String) : Item(displayName, null)
-
-@ApiStatus.Internal
-abstract class Target(displayName: @NlsContexts.Label String, icon: Icon, val targetName: String) : Item(displayName, icon)
+abstract class Target(
+  displayName: @NlsContexts.Label String,
+  icon: Icon,
+  val targetName: String,
+  separator: @NlsContexts.Separator String? = null,
+) : Item(displayName, icon, separator)
 
 @ApiStatus.Internal
 class SavedTarget(private val config: TargetEnvironmentConfiguration) :
@@ -63,13 +68,66 @@ class SavedTarget(private val config: TargetEnvironmentConfiguration) :
  * Represents the "local machine" target.
  */
 @ApiStatus.Internal
-class LocalTarget : Target(ExecutionBundle.message("local.machine"), AllIcons.Nodes.HomeFolder, LocalTargetType.LOCAL_TARGET_NAME)
+class LocalTarget(separator: @NlsContexts.Separator String? = null) :
+  Target(ExecutionBundle.message("local.machine"), AllIcons.Nodes.HomeFolder, LocalTargetType.LOCAL_TARGET_NAME, separator)
 
 @ApiStatus.Internal
-class Type<T : TargetEnvironmentConfiguration>(private val type: TargetEnvironmentType<T>) :
-  Item(ExecutionBundle.message("run.on.targets.label.new.target.of.type", type.displayName), type.icon) {
+class Type<T : TargetEnvironmentConfiguration>(private val type: TargetEnvironmentType<T>, separator: @NlsContexts.Separator String?) :
+  Item(ExecutionBundle.message("run.on.targets.label.new.target.of.type", type.displayName), type.icon, separator) {
 
   fun createWizard(project: Project, languageRuntime: LanguageRuntimeType<*>?): TargetEnvironmentWizard? {
     return TargetEnvironmentWizard.createWizard(project, type, languageRuntime)
+  }
+}
+
+/**
+ * We render the project default target item as "Local Machine" without "Project Default" prefix if we do not have any saved targets in
+ * the list.
+ *
+ * We cannot use the size of the model explicitly in {@code customizeCellRenderer(...)} method to determine whether there are
+ * "Saved targets" items because the model also contains "New Target" section with the corresponding items.
+ */
+internal fun createItemRenderer(
+  hasSavedTargetsSupplier: Supplier<Boolean>,
+  projectDefaultTarget: TargetEnvironmentConfiguration?,
+): ListCellRenderer<Item?> {
+  val projectDefaultTargetItem = if (projectDefaultTarget != null) SavedTarget(projectDefaultTarget) else null
+
+  return listCellRenderer {
+    value?.separator?.let {
+      separator {
+        text = it
+      }
+    }
+
+    val value = value
+    if (value != null) {
+      value.icon?.let { icon(it) }
+      text(value.displayName)
+      return@listCellRenderer
+    }
+
+    when {
+      !hasSavedTargetsSupplier.get() -> {
+        icon(AllIcons.Nodes.HomeFolder)
+        text(ExecutionBundle.message("local.machine"))
+      }
+
+      projectDefaultTargetItem == null -> {
+        icon(AllIcons.Nodes.HomeFolder)
+        text(ExecutionBundle.message("targets.details.project.default"))
+        text(ExecutionBundle.message("local.machine")) {
+          foreground = greyForeground
+        }
+      }
+
+      else -> {
+        projectDefaultTargetItem.icon?.let { icon(it) }
+        text(ExecutionBundle.message("targets.details.project.default"))
+        text(projectDefaultTargetItem.displayName) {
+          foreground = greyForeground
+        }
+      }
+    }
   }
 }
