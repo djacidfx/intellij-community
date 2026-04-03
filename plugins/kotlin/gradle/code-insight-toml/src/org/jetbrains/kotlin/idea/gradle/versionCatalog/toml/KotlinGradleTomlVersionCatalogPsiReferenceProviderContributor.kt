5d34a85c1356f20e876787b7b75822d4c7e97480
Nikita Biriukov <nikita.biriukov@jetbrains.com>
2026-04-03 11:42:20 +0200

    [gradle] IDEA-302835 remove module intellij.kotlin.gradle.codeInsight.toml.k2
    Its code is not K2-dependent anymore and could be located in the parent module:
    * KotlinGradleTomlVersionCatalogPsiReferenceProviderContributor
    
    ### Tests:
    * KotlinGradleVersionCatalogNavigationTest
    * KotlinGradleVersionCatalogRenameTest
    * KotlinGradleFindUsagesTest
    
    GitOrigin-RevId: d44b1e3866b2a97020354fa371ec053f778a8371

--- plugins/kotlin/gradle/code-insight-toml/src/org/jetbrains/kotlin/idea/gradle/versionCatalog/toml/KotlinGradleTomlVersionCatalogPsiReferenceProviderContributor.kt
+++ plugins/kotlin/gradle/code-insight-toml/src/org/jetbrains/kotlin/idea/gradle/versionCatalog/toml/KotlinGradleTomlVersionCatalogPsiReferenceProviderContributor.kt
@@ -1,19 +1,16 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.kotlin.idea.gradle.versionCatalog.toml.k2
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.gradle.versionCatalog.toml
 
 import com.intellij.gradle.java.toml.findTomlFile
-import org.jetbrains.kotlin.idea.gradle.versionCatalog.toml.KtTomlVersionCatalogReference
-import org.jetbrains.kotlin.idea.gradle.versionCatalog.toml.matchesTopmostCatalogReferencePattern
 import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
 import org.jetbrains.kotlin.references.KotlinPsiReferenceProviderContributor
-import org.jetbrains.kotlin.references.KotlinPsiReferenceProviderContributor.ReferenceProvider
 
-internal class K2GradleTomlVersionCatalogPsiReferenceProviderContributor : KotlinPsiReferenceProviderContributor<KtDotQualifiedExpression> {
+internal class KotlinGradleTomlVersionCatalogPsiReferenceProviderContributor : KotlinPsiReferenceProviderContributor<KtDotQualifiedExpression> {
     override val elementClass: Class<KtDotQualifiedExpression>
         get() = KtDotQualifiedExpression::class.java
 
-    override val referenceProvider: ReferenceProvider<KtDotQualifiedExpression>
-        get() = ReferenceProvider { dotExpression ->
+    override val referenceProvider: KotlinPsiReferenceProviderContributor.ReferenceProvider<KtDotQualifiedExpression>
+        get() = KotlinPsiReferenceProviderContributor.ReferenceProvider { dotExpression ->
             val tomlFile = when {
                 !dotExpression.containingFile.name.endsWith(".gradle.kts") -> null
                 !dotExpression.matchesTopmostCatalogReferencePattern() -> null
