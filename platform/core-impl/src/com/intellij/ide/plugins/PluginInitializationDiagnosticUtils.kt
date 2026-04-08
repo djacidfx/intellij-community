1112e86a53dd56574f39ce8bc5fc7f0b4dc79eb8
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2026-04-08 18:41:03 +0200

    IJPL-236591 initializePlugins: extract exclusion tree logger to PluginInitializationDiagnosticUtils
    GitOrigin-RevId: 93c8b20660d1f5ebecf59a56655c90bec5e79e3d

--- platform/core-impl/src/com/intellij/ide/plugins/PluginInitializationDiagnosticUtils.kt
+++ platform/core-impl/src/com/intellij/ide/plugins/PluginInitializationDiagnosticUtils.kt
@@ -1,14 +1,164 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.plugins
 
+import com.intellij.ide.plugins.PluginDependencyAnalysis.DependencyRef
 import com.intellij.ide.plugins.PluginManagerCore.logger
 import com.intellij.ide.plugins.PluginManagerCore.oldPluginSetBuilder
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.extensions.PluginId
+import com.intellij.util.asSafely
 import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Internal
 object PluginInitializationDiagnosticUtils {
+  fun logExclusionTree(resolvedPluginSet: ResolvedPluginSet, incompletePlugins: HashMap<PluginId, PluginMainDescriptor>) {
+    val broadResolveContext by lazy { AmbiguousPluginSet.build(resolvedPluginSet.originalPluginSet.plugins + incompletePlugins.values) }
+    val exclusionChildren = LinkedHashMap<IdeaPluginDescriptorImpl, ArrayList<IdeaPluginDescriptorImpl>>()
+    val roots = LinkedHashSet<IdeaPluginDescriptorImpl>()
+    for (plugin in resolvedPluginSet.originalPluginSet.plugins) {
+      for (descriptor in plugin.sequenceAllDescriptors()) {
+        if (resolvedPluginSet.isResolved(descriptor)) continue
+        val chain = descriptor.sequenceDescriptorExclusionChain(resolvedPluginSet::getExclusionReason).take(2).toList()
+        if (chain.size == 2) {
+          val precedingExclusion = chain[1]
+          val precedingReason = resolvedPluginSet.getExclusionReason(precedingExclusion)
+          if (precedingReason is PartOfDependencyCycle || precedingReason is PartOfRuntimeModuleGroupDependencyCycle) {
+            val key = precedingReason.getDependencyCycleRepresentative()
+            exclusionChildren.getOrPut(key) { ArrayList() }.add(descriptor) // attach chained exclusions to a cycle representative only, so they all are grouped up
+          }
+          else {
+            exclusionChildren.getOrPut(precedingExclusion) { ArrayList() }.add(descriptor)
+          }
+        } else if (chain.size == 1) {
+          val exclusionReason = resolvedPluginSet.getExclusionReason(descriptor)
+          val shouldAddRoot = if (exclusionReason is PartOfDependencyCycle || exclusionReason is PartOfRuntimeModuleGroupDependencyCycle) {
+            exclusionReason.getDependencyCycleRepresentative() == descriptor
+          } else {
+            true
+          }
+          if (shouldAddRoot) roots.add(descriptor)
+        }
+      }
+    }
+
+    fun StringBuilder.appendIndentString(indent: Int) {
+      repeat(indent) { append("  ") }
+      if (indent > 0) append("└ ")
+    }
+
+    fun StringBuilder.writeExclusionTree(descriptor: IdeaPluginDescriptorImpl, indent: Int) {
+      val exclusionReason = resolvedPluginSet.getExclusionReason(descriptor)!!
+      appendIndentString(indent)
+      appendLine(exclusionReason.logMessage())
+      val children = exclusionChildren[descriptor] ?: emptyList()
+      val (childFreeChainedExclusions, otherChildren) = children.partition { (exclusionChildren[it]?.size ?: 0) == 0 && resolvedPluginSet.getExclusionReason(it) is ChainedExclusion }
+      if (childFreeChainedExclusions.isNotEmpty()) {
+        appendIndentString(indent + 1)
+        appendLine("dependent ${childFreeChainedExclusions.joinToString(", ") { it.getLogDescription() }} excluded")
+      }
+      for (child in otherChildren) {
+        writeExclusionTree(child, indent + 1)
+      }
+    }
+
+    val dependencyIsNotResolvedRoots = roots.asSequence().filter { resolvedPluginSet.getExclusionReason(it) is DependencyIsNotResolved }.toSet()
+    roots.removeAll(dependencyIsNotResolvedRoots)
+    val logHeader = "Plugin set resolution:\n"
+    val logBuilder = StringBuilder().apply {
+      append(logHeader)
+      dependencyIsNotResolvedRoots.map { resolvedPluginSet.getExclusionReason(it) as DependencyIsNotResolved }.groupBy { it.dependency }
+        .forEach { (ref, roots) ->
+          val disabledPlugin = broadResolveContext.resolveReference(ref).firstOrNull { resolvedPluginSet.initContext.isPluginDisabled(it.pluginId) }
+          if (disabledPlugin != null) {
+            appendLine("${disabledPlugin.getLogDescription()} is marked disabled")
+          } else {
+            when (ref) {
+              is DependencyRef.ContentModule -> append("module ")
+              is DependencyRef.Plugin -> append("plugin ")
+            }
+            appendLine("${ref.getIdString()} is not resolved")
+          }
+
+          // a bit of duplication, but I guess it's alright for this code
+          val (childFreeExclusions, otherRoots) = roots.partition { (exclusionChildren[it.descriptor]?.size ?: 0) == 0 }
+          if (childFreeExclusions.isNotEmpty()) {
+            appendIndentString(1)
+            appendLine("dependent ${childFreeExclusions.joinToString(", ") { it.descriptor.getLogDescription() }} excluded")
+          }
+          for (root in otherRoots) {
+            writeExclusionTree(root.descriptor, 1)
+          }
+        }
+      for (root in roots) {
+        writeExclusionTree(root, 0)
+      }
+    }
+    if (logBuilder.last() == '\n') {
+      logBuilder.setLength(logBuilder.length - 1)
+    }
+    if (logBuilder.length == logHeader.length - 1) {
+      logBuilder.append(" no exclusions")
+    }
+    logger.warn(logBuilder.toString())
+  }
+
+  private fun DescriptorExclusionReason.getDependencyCycleRepresentative(): IdeaPluginDescriptorImpl =
+    asSafely<PartOfDependencyCycle>()?.dependencyCycle?.nodesWithDependenciesOnCycle?.keys?.first()
+    ?: asSafely<PartOfRuntimeModuleGroupDependencyCycle>()?.dependencyCycle?.nodesWithDependenciesOnCycle?.keys?.first()?.representativeModule
+    ?: error("$this is not a cycle exclusion reason")
+
+  private fun DescriptorExclusionReason.logMessage(): String {
+    val logDescr = descriptor.getLogDescription()
+    return when (this) {
+      // chained:
+      is ContentModuleParentIsExcluded -> "dependent $logDescr excluded"
+      is RequiredContentModuleIsExcluded -> "dependent $logDescr excluded"
+      is DependencyIsExcluded -> "dependent $logDescr excluded"
+      is DependsParentIsExcluded -> "dependent $logDescr excluded"
+      // root:
+      is DependencyIsNotResolved -> "dependent $logDescr excluded" // special handling in logExclusionTree
+      is DependencyIsNotVisible -> "$logDescr depends on ${dependencyModule.getLogDescription()} which is not visible"
+      is ExcludedByEnvironmentConfiguration -> "$logDescr is excluded: ${reason.logMessage}"
+      is IncompatibleWithAnotherModule -> "$logDescr is incompatible with ${preferredIncompatibleModule.getLogDescription()}"
+      is PackagePrefixConflictWithAnotherModule -> "$logDescr declares the same package prefix as in ${preferredConflictingModule.getLogDescription()}"
+      is PartOfDependencyCycle -> buildString {
+        appendLine("The following modules form a dependency cycle:")
+        explainCycle(dependencyCycle, fmtNode = { it.getLogDescription() })
+      }
+      is PartOfRuntimeModuleGroupDependencyCycle -> buildString {
+        appendLine("Classloaders made from the following groups form a dependency cycle:")
+        explainCycle(
+          dependencyCycle,
+          fmtNode = { "${it.representativeModule.getLogDescription()} (${it.sortedDescriptors.joinToString { it.getLogDescription() }})" },
+          fmtDeps = { it.joinToString(", ") { it.representativeModule.getLogDescription() } }
+        )
+      }
+      is ProductRulesImposedExclusion -> "$logDescr is excluded by product rules: ${this.productReason}"
+    }
+  }
+
+  private fun <N> StringBuilder.explainCycle(cycle: DependencyCycleInfo<N>, fmtNode: (N) -> String, fmtDeps: (List<N>) -> String = { it.joinToString(", ") { fmtNode(it) }}) {
+    var endLine = false
+    cycle.nodesWithDependenciesOnCycle.forEach { (node, dependencies) ->
+      if (endLine) appendLine()
+      else endLine = true
+      append("    | ${fmtNode(node)} depends on: ${fmtDeps(dependencies)}")
+    }
+  }
+
+  private fun IdeaPluginDescriptorImpl.getLogDescription(): String {
+    return when (this) {
+      is PluginMainDescriptor -> "plugin '$name' ($pluginId, $version)"
+      is DependsSubDescriptor -> "<depends> config '${descriptorPath}' of plugin ${pluginId}"
+      is ContentModuleDescriptor -> "module ${moduleId}"
+    }
+  }
+
+  internal fun DependencyRef.getIdString(): String = when (this) {
+    is DependencyRef.ContentModule -> moduleId.name
+    is DependencyRef.Plugin -> pluginId.idString
+  }
+
   fun runNewPluginSetDiagnosticsIfNeeded(
     initContext: PluginInitializationContext,
     pluginsToLoad: UnambiguousPluginSet,
--- platform/core-impl/src/com/intellij/ide/plugins/PluginManagerCore.kt
+++ platform/core-impl/src/com/intellij/ide/plugins/PluginManagerCore.kt
@@ -8,7 +8,7 @@ import com.intellij.diagnostic.Activity
 import com.intellij.diagnostic.CoroutineTracerShim
 import com.intellij.diagnostic.LoadingState
 import com.intellij.ide.plugins.DisabledPluginsState.Companion.invalidate
-import com.intellij.ide.plugins.PluginDependencyAnalysis.DependencyRef
+import com.intellij.ide.plugins.PluginInitializationDiagnosticUtils.getIdString
 import com.intellij.ide.plugins.PluginManagerCore.ULTIMATE_PLUGIN_ID
 import com.intellij.ide.plugins.PluginManagerCore.getPluginSet
 import com.intellij.ide.plugins.PluginManagerCore.isDisabled
@@ -29,7 +29,6 @@ import com.intellij.openapi.util.IntellijInternalApi
 import com.intellij.openapi.util.NlsSafe
 import com.intellij.openapi.util.text.HtmlChunk
 import com.intellij.util.PlatformUtils
-import com.intellij.util.asSafely
 import com.intellij.util.lang.ZipEntryResolverPool
 import com.intellij.util.system.CpuArch
 import com.intellij.util.system.OS
@@ -614,7 +613,7 @@ object PluginManagerCore {
     }
     else {
       val resolvedPluginSet = initContext.resolveConstraints(pluginsToLoad)
-      resolvedPluginSet.logExclusionTree(incompletePlugins)
+      PluginInitializationDiagnosticUtils.logExclusionTree(resolvedPluginSet, incompletePlugins)
       val (adaptedPluginSet, cycleErrors) = adaptResolvedPluginSetAsOldPluginSet(
         resolvedPluginSet = resolvedPluginSet,
         incompletePlugins = incompletePlugins,
@@ -1128,154 +1127,6 @@ object PluginManagerCore {
   }
   //</editor-fold>
 }
-
-private fun ResolvedPluginSet.logExclusionTree(incompletePlugins: HashMap<PluginId, PluginMainDescriptor>) {
-  val broadResolveContext by lazy { AmbiguousPluginSet.build(this.originalPluginSet.plugins + incompletePlugins.values) }
-  val exclusionChildren = LinkedHashMap<IdeaPluginDescriptorImpl, ArrayList<IdeaPluginDescriptorImpl>>()
-  val roots = LinkedHashSet<IdeaPluginDescriptorImpl>()
-  for (plugin in originalPluginSet.plugins) {
-    for (descriptor in plugin.sequenceAllDescriptors()) {
-      if (isResolved(descriptor)) continue
-      val chain = descriptor.sequenceDescriptorExclusionChain(this::getExclusionReason).take(2).toList()
-      if (chain.size == 2) {
-        val precedingExclusion = chain[1]
-        val precedingReason = getExclusionReason(precedingExclusion)
-        if (precedingReason is PartOfDependencyCycle || precedingReason is PartOfRuntimeModuleGroupDependencyCycle) {
-          val key = precedingReason.getDependencyCycleRepresentative()
-          exclusionChildren.getOrPut(key) { ArrayList() }.add(descriptor) // attach chained exclusions to a cycle representative only, so they all are grouped up
-        }
-        else {
-          exclusionChildren.getOrPut(precedingExclusion) { ArrayList() }.add(descriptor)
-        }
-      } else if (chain.size == 1) {
-        val exclusionReason = getExclusionReason(descriptor)
-        val shouldAddRoot = if (exclusionReason is PartOfDependencyCycle || exclusionReason is PartOfRuntimeModuleGroupDependencyCycle) {
-          exclusionReason.getDependencyCycleRepresentative() == descriptor
-        } else {
-          true
-        }
-        if (shouldAddRoot) roots.add(descriptor)
-      }
-    }
-  }
-
-  fun StringBuilder.appendIndentString(indent: Int) {
-    repeat(indent) { append("  ") }
-    if (indent > 0) append("└ ")
-  }
-
-  fun StringBuilder.writeExclusionTree(descriptor: IdeaPluginDescriptorImpl, indent: Int) {
-    val exclusionReason = getExclusionReason(descriptor)!!
-    appendIndentString(indent)
-    appendLine(exclusionReason.logMessage())
-    val children = exclusionChildren[descriptor] ?: emptyList()
-    val (childFreeChainedExclusions, otherChildren) = children.partition { (exclusionChildren[it]?.size ?: 0) == 0 && getExclusionReason(it) is ChainedExclusion }
-    if (childFreeChainedExclusions.isNotEmpty()) {
-      appendIndentString(indent + 1)
-      appendLine("dependent ${childFreeChainedExclusions.joinToString(", ") { it.getLogDescription() }} excluded")
-    }
-    for (child in otherChildren) {
-      writeExclusionTree(child, indent + 1)
-    }
-  }
-  val dependencyIsNotResolvedRoots = roots.asSequence().filter { getExclusionReason(it) is DependencyIsNotResolved }.toSet()
-  roots.removeAll(dependencyIsNotResolvedRoots)
-  val logHeader = "Plugin set resolution:\n"
-  val logBuilder = StringBuilder().apply {
-    append(logHeader)
-    dependencyIsNotResolvedRoots.map { getExclusionReason(it) as DependencyIsNotResolved }.groupBy { it.dependency }
-      .forEach { (ref, roots) ->
-        val disabledPlugin = broadResolveContext.resolveReference(ref).firstOrNull { initContext.isPluginDisabled(it.pluginId) }
-        if (disabledPlugin != null) {
-          appendLine("${disabledPlugin.getLogDescription()} is marked disabled")
-        } else {
-          when (ref) {
-            is DependencyRef.ContentModule -> append("module ")
-            is DependencyRef.Plugin -> append("plugin ")
-          }
-          appendLine("${ref.getIdString()} is not resolved")
-        }
-
-        // a bit of duplication, but I guess it's alright for this code
-        val (childFreeExclusions, otherRoots) = roots.partition { (exclusionChildren[it.descriptor]?.size ?: 0) == 0 }
-        if (childFreeExclusions.isNotEmpty()) {
-          appendIndentString(1)
-          appendLine("dependent ${childFreeExclusions.joinToString(", ") { it.descriptor.getLogDescription() }} excluded")
-        }
-        for (root in otherRoots) {
-          writeExclusionTree(root.descriptor, 1)
-        }
-      }
-    for (root in roots) {
-      writeExclusionTree(root, 0)
-    }
-  }
-  if (logBuilder.last() == '\n') {
-    logBuilder.setLength(logBuilder.length - 1)
-  }
-  if (logBuilder.length == logHeader.length - 1) {
-    logBuilder.append(" no exclusions")
-  }
-  PluginManagerCore.logger.warn(logBuilder.toString())
-}
-
-private fun DescriptorExclusionReason.getDependencyCycleRepresentative(): IdeaPluginDescriptorImpl =
-  asSafely<PartOfDependencyCycle>()?.dependencyCycle?.nodesWithDependenciesOnCycle?.keys?.first()
-  ?: asSafely<PartOfRuntimeModuleGroupDependencyCycle>()?.dependencyCycle?.nodesWithDependenciesOnCycle?.keys?.first()?.representativeModule
-  ?: error("$this is not a cycle exclusion reason")
-
-private fun DescriptorExclusionReason.logMessage(): String {
-  val logDescr = descriptor.getLogDescription()
-  return when (this) {
-    // chained:
-    is ContentModuleParentIsExcluded -> "dependent $logDescr excluded"
-    is RequiredContentModuleIsExcluded -> "dependent $logDescr excluded"
-    is DependencyIsExcluded -> "dependent $logDescr excluded"
-    is DependsParentIsExcluded -> "dependent $logDescr excluded"
-    // root:
-    is DependencyIsNotResolved -> "dependent $logDescr excluded" // special handling in logExclusionTree
-    is DependencyIsNotVisible -> "$logDescr depends on ${dependencyModule.getLogDescription()} which is not visible"
-    is ExcludedByEnvironmentConfiguration -> "$logDescr is excluded: ${reason.logMessage}"
-    is IncompatibleWithAnotherModule -> "$logDescr is incompatible with ${preferredIncompatibleModule.getLogDescription()}"
-    is PackagePrefixConflictWithAnotherModule -> "$logDescr declares the same package prefix as in ${preferredConflictingModule.getLogDescription()}"
-    is PartOfDependencyCycle -> buildString {
-      appendLine("The following modules form a dependency cycle:")
-      explainCycle(dependencyCycle, IdeaPluginDescriptorImpl::getLogDescription)
-    }
-    is PartOfRuntimeModuleGroupDependencyCycle -> buildString {
-      appendLine("Classloaders made from the following groups form a dependency cycle:")
-      explainCycle(
-        dependencyCycle,
-        fmtNode = { "${it.representativeModule.getLogDescription()} (${it.sortedDescriptors.joinToString { it.getLogDescription() }})" },
-        fmtDeps = { it.joinToString(", ") { it.representativeModule.getLogDescription() } }
-      )
-    }
-    is ProductRulesImposedExclusion -> "$logDescr is excluded by product rules: ${this.productReason}"
-  }
-}
-
-private fun <N> StringBuilder.explainCycle(cycle: DependencyCycleInfo<N>, fmtNode: (N) -> String, fmtDeps: (List<N>) -> String = { it.joinToString(", ") { fmtNode(it) }}) {
-  var endLine = false
-  cycle.nodesWithDependenciesOnCycle.forEach { (node, dependencies) ->
-    if (endLine) appendLine()
-    else endLine = true
-    append("    | ${fmtNode(node)} depends on: ${fmtDeps(dependencies)}")
-  }
-}
-
-private fun IdeaPluginDescriptorImpl.getLogDescription(): String {
-  return when (this) {
-    is PluginMainDescriptor -> "plugin '$name' ($pluginId, $version)"
-    is DependsSubDescriptor -> "<depends> config '${descriptorPath}' of plugin ${pluginId}"
-    is ContentModuleDescriptor -> "module ${moduleId}"
-  }
-}
-
-private fun DependencyRef.getIdString(): String = when (this) {
-  is DependencyRef.ContentModule -> moduleId.name
-  is DependencyRef.Plugin -> pluginId.idString
-}
-
 
 private fun processAllNonOptionalDependencies(
   rootDescriptor: IdeaPluginDescriptorImpl,
