8de701f35be4bbb5f8fc2c2820185ea1834fe78a
Jakob Gerhardt <jakob.gerhardt@jetbrains.com>
2026-04-13 11:51:38 +0200

    IJPL-240179 remove duplicate Analyzer definition
    GitOrigin-RevId: 126007780aa57f08cfd90689da8724012783a574

--- plugins/search-everywhere-lucene/backend/test/providers/files/analysis/AbbreviationTokenFilterTest.kt
+++ plugins/search-everywhere-lucene/backend/test/providers/files/analysis/AbbreviationTokenFilterTest.kt
@@ -17,7 +17,7 @@ import org.junit.jupiter.api.Test
  */
 class AbbreviationTokenFilterTest : AnalyzersTestBase() {
 
-  private fun abbreviationAnalyzer(): Analyzer = object : Analyzer() {
+  private fun abbreviationAnalyzer(allowedSkip: Int = 0): Analyzer = object : Analyzer() {
     override fun createComponents(fieldName: String): TokenStreamComponents {
       val tokenizer = KeywordTokenizer()
       var stream: TokenStream = tokenizer
@@ -37,6 +37,7 @@ class AbbreviationTokenFilterTest : AnalyzersTestBase() {
       stream = AbbreviationTokenFilter(stream,
                                        sourceTypes = setOf(FileTokenType.FILENAME_PART),
                                        outputType = FileTokenType.FILENAME_ABBREVIATION,
+                                       allowedSkip = allowedSkip,
                                        skipOutputType = FileTokenType.FILENAME_ABBREVIATION_WITH_SKIPS)
       return TokenStreamComponents(tokenizer, stream)
     }
@@ -79,7 +80,7 @@ class AbbreviationTokenFilterTest : AnalyzersTestBase() {
   fun `multiple abbreviations with allowedSkip`() {
     // SearchEveryWhereUI -> [Search, Every, Where, UI], allowedSkip=1, minLength=2
     // size=4: "sewu"; size=3: "sew"(skip UI), "seu"(skip Where), "swu"(skip Every), "ewu"(skip Search)
-    tokenizing(abbreviationAnalyzerWithSkip(1), "SearchEveryWhereUI")
+    tokenizing(abbreviationAnalyzer(1), "SearchEveryWhereUI")
       .producesToken("sewu", FileTokenType.FILENAME_ABBREVIATION)
       .producesToken("sew", FileTokenType.FILENAME_ABBREVIATION_WITH_SKIPS)
       .producesToken("seu", FileTokenType.FILENAME_ABBREVIATION_WITH_SKIPS)
@@ -88,28 +89,13 @@ class AbbreviationTokenFilterTest : AnalyzersTestBase() {
       .noDuplicateTokens()
   }
 
-  private fun abbreviationAnalyzerWithSkip(allowedSkip: Int): Analyzer = object : Analyzer() {
-    override fun createComponents(fieldName: String): TokenStreamComponents {
-      val tokenizer = KeywordTokenizer()
-      var stream: TokenStream = tokenizer
-      stream = object : org.apache.lucene.analysis.TokenFilter(stream) {
-        private val multiTypeAttr = addAttribute(MultiTypeAttribute::class.java)
-        override fun incrementToken(): Boolean {
-          if (!input.incrementToken()) return false
-          multiTypeAttr.clearTypes().setTypes(setOf(FileTokenType.FILENAME))
-          return true
-        }
-      }
-      stream = WordSplittingTokenFilter(stream,
-                                        inputTypes = setOf(FileTokenType.FILENAME),
-                                        outputType = FileTokenType.FILENAME_PART,
-                                        passThrough = PassthroughOptions.PassthroughLast)
-      stream = AbbreviationTokenFilter(stream,
-                                       sourceTypes = setOf(FileTokenType.FILENAME_PART),
-                                       outputType = FileTokenType.FILENAME_ABBREVIATION,
-                                       allowedSkip = allowedSkip,
-                                       skipOutputType = FileTokenType.FILENAME_ABBREVIATION_WITH_SKIPS)
-      return TokenStreamComponents(tokenizer, stream)
-    }
+  @Test
+  fun `snake case abbreviation for two-part name`() {
+    tokenizing(abbreviationAnalyzer(), "my_file")
+      .print()
+      .producesToken("mf", FileTokenType.FILENAME_ABBREVIATION)
+      .producesToken("my", FileTokenType.FILENAME_PART, 0, 2)
+      .producesToken("file", FileTokenType.FILENAME_PART, 3, 7)
+      .noDuplicateTokens()
   }
 }
