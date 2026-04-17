b3aa5a712d8a9ecb6bbf6511636e39a4220b49e9
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2026-04-17 13:09:19 +0300

    [UI, Run Widget] IJPL-61108 Refactoring: extract ActionButtonWithText.layout
    Rationale: this way we can override it to customize
    the layout for the run widget, so that the name is shortened
    properly. It's likely that we'll only need to customize the text
    only, but we make it @Internal, so it won't hurt to have
    extra parameters here.
    
    GitOrigin-RevId: 87be144ee33b41acd9700dd49f8a3336f983f636

--- platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/ActionButtonWithText.java
+++ platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/ActionButtonWithText.java
@@ -30,6 +30,7 @@ import com.intellij.util.ui.StartupUiUtil;
 import com.intellij.util.ui.UIUtil;
 import com.intellij.util.ui.UIUtilities;
 import org.intellij.lang.annotations.MagicConstant;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -154,7 +155,6 @@ public class ActionButtonWithText extends ActionButton {
     Dimension basicSize = super.getPreferredSize();
 
     Icon icon = getIcon();
-    int position = horizontalTextPosition();
 
     FontMetrics fm = getFontMetrics(getFont());
     Rectangle viewRect = new Rectangle(0, 0, Short.MAX_VALUE, Short.MAX_VALUE);
@@ -164,10 +164,7 @@ public class ActionButtonWithText extends ActionButton {
 
     Rectangle iconR = new Rectangle();
     Rectangle textR = new Rectangle();
-    SwingUtilities.layoutCompoundLabel(this, fm, getText(), icon,
-                                       SwingConstants.CENTER, horizontalTextAlignment(),
-                                       SwingConstants.CENTER, position,
-                                       viewRect, iconR, textR, iconTextSpace());
+    layout(fm, getText(), icon, viewRect, iconR, textR);
     int x1 = Math.min(iconR.x, textR.x);
     int x2 = Math.max(iconR.x + iconR.width, textR.x + textR.width);
     int y1 = Math.min(iconR.y, textR.y);
@@ -184,6 +181,21 @@ public class ActionButtonWithText extends ActionButton {
     rv.width = Math.max(rv.width, basicSize.width);
     rv.height = Math.max(rv.height, basicSize.height);
     return rv;
+  }
+  
+  @ApiStatus.Internal
+  protected @NotNull @NlsActions.ActionText String layout(
+    @NotNull FontMetrics fm,
+    @NotNull @NlsActions.ActionText String fullText,
+    @Nullable Icon icon,
+    @NotNull Rectangle inViewRect,
+    @NotNull Rectangle outIconRect,
+    @NotNull Rectangle outTextRect
+  ) {
+    return SwingUtilities.layoutCompoundLabel(this, fm, fullText, icon,
+                                              SwingConstants.CENTER, horizontalTextAlignment(),
+                                              SwingConstants.CENTER, horizontalTextPosition(),
+                                              inViewRect, outIconRect, outTextRect, iconTextSpace());
   }
 
   @Override
@@ -222,10 +234,7 @@ public class ActionButtonWithText extends ActionButton {
 
     Rectangle iconRect = new Rectangle();
     Rectangle textRect = new Rectangle();
-    String text = SwingUtilities.layoutCompoundLabel(this, fm, getText(), icon,
-                                                     SwingConstants.CENTER, horizontalTextAlignment(),
-                                                     SwingConstants.CENTER, horizontalTextPosition(),
-                                                     viewRect, iconRect, textRect, iconTextSpace());
+    String text = layout(fm, getText(), icon, viewRect, iconRect, textRect);
     if (arrowIcon != null) {
       int alignment = horizontalTextAlignment();
       int dx = alignment == SwingConstants.CENTER ? arrowIcon.getIconWidth() / 2 - 2:
