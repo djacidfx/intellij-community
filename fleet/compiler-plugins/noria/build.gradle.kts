ea6b51f93ca67942fae65b0b49ea9cbdaa44216d
Dmitriy.Panov <dmitriy.panov@jetbrains.com>
2026-04-17 13:53:08 +0200

    [build] IJPL-242610 update kotlin compiler plugins to 2.3.21-RC2
    IJ-MR-200186
    
    GitOrigin-RevId: ea9dbfd2c6f2d503d6833cb8652e525c9ef7aec7

--- fleet/compiler-plugins/noria/build.gradle.kts
+++ fleet/compiler-plugins/noria/build.gradle.kts
@@ -18,7 +18,7 @@ val pluginVersion = run {
 }
 
 // the compiler plugin will be used together with this Kotlin compiler
-val KOTLIN_VERSION = "2.3.21-RC"
+val KOTLIN_VERSION = "2.3.21-RC2"
 
 // the compiler plugin will be built with these Kotlin LV/APIV
 val KOTLIN_LANGUAGE_VERSION = "2.3"
