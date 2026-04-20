4b387a6117c839fbf84cb4db1162a998aa3d990d
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2026-04-20 12:22:46 +0300

    [UI, Run Widget] IJPL-61108 Extract SwingTextTrimmerStrategy
    To be able to use smart trimming for the run config popup,
    we need to be able to apply it to the renderer label.
    Thankfully, we already have SwingTextTrimmer for that,
    but it only supports a fixed set of parameters.
    Let's make it more flexible by adding a customizable
    strategy to it.
    
    GitOrigin-RevId: 657bc38151f70da00a16c3ebd22c7359f3b20bd7

--- platform/platform-impl/src/com/intellij/util/ui/SwingTextTrimmer.java
+++ platform/platform-impl/src/com/intellij/util/ui/SwingTextTrimmer.java
@@ -10,45 +10,31 @@ import javax.swing.SwingConstants;
 import java.awt.FontMetrics;
 
 public class SwingTextTrimmer {
-  public static final SwingTextTrimmer ELLIPSIS_AT_LEFT = new SwingTextTrimmer(StringUtil.ELLIPSIS, SwingConstants.LEFT);
-  public static final SwingTextTrimmer ELLIPSIS_AT_RIGHT = new SwingTextTrimmer(StringUtil.ELLIPSIS, SwingConstants.RIGHT);
-  public static final SwingTextTrimmer ELLIPSIS_IN_CENTER = new SwingTextTrimmer(StringUtil.ELLIPSIS, SwingConstants.CENTER);
-  public static final SwingTextTrimmer THREE_DOTS_AT_LEFT = new SwingTextTrimmer(StringUtil.THREE_DOTS, SwingConstants.LEFT);
-  public static final SwingTextTrimmer THREE_DOTS_AT_RIGHT = new SwingTextTrimmer(StringUtil.THREE_DOTS, SwingConstants.RIGHT);
-  public static final SwingTextTrimmer THREE_DOTS_IN_CENTER = new SwingTextTrimmer(StringUtil.THREE_DOTS, SwingConstants.CENTER);
+  public static final SwingTextTrimmer ELLIPSIS_AT_LEFT = new SwingTextTrimmer(new StandardTrimmerStrategy(StringUtil.ELLIPSIS, SwingConstants.LEFT));
+  public static final SwingTextTrimmer ELLIPSIS_AT_RIGHT = new SwingTextTrimmer(new StandardTrimmerStrategy(StringUtil.ELLIPSIS, SwingConstants.RIGHT));
+  public static final SwingTextTrimmer ELLIPSIS_IN_CENTER = new SwingTextTrimmer(new StandardTrimmerStrategy(StringUtil.ELLIPSIS, SwingConstants.CENTER));
+  public static final SwingTextTrimmer THREE_DOTS_AT_LEFT = new SwingTextTrimmer(new StandardTrimmerStrategy(StringUtil.THREE_DOTS, SwingConstants.LEFT));
+  public static final SwingTextTrimmer THREE_DOTS_AT_RIGHT = new SwingTextTrimmer(new StandardTrimmerStrategy(StringUtil.THREE_DOTS, SwingConstants.RIGHT));
+  public static final SwingTextTrimmer THREE_DOTS_IN_CENTER = new SwingTextTrimmer(new StandardTrimmerStrategy(StringUtil.THREE_DOTS, SwingConstants.CENTER));
 
   public static final Key<SwingTextTrimmer> KEY = Key.create(SwingTextTrimmer.class.getSimpleName());
 
   public static SwingTextTrimmer createCenterTrimmer(float ratio) {
-    return new SwingTextTrimmer(StringUtil.ELLIPSIS, SwingConstants.CENTER, ratio);
+    return new SwingTextTrimmer(new StandardTrimmerStrategy(StringUtil.ELLIPSIS, SwingConstants.CENTER, ratio));
   }
 
-  private final String ellipsis;
-  private final int alignment;
-  private final float ratio;
+  private final @NotNull SwingTextTrimmerStrategy strategy;
   private boolean trimmed;
 
-  SwingTextTrimmer(@NotNull String ellipsis, int alignment) {
-    this(ellipsis, alignment, 0.5f);
-  }
-
-  SwingTextTrimmer(@NotNull String ellipsis, int alignment, float ratio) {
-    this.ellipsis = ellipsis;
-    this.alignment = alignment;
-    this.ratio = ratio;
+  SwingTextTrimmer(@NotNull SwingTextTrimmerStrategy strategy) {
+    this.strategy = strategy;
   }
 
   public @NotNull String trim(@Nullable String text, @NotNull FontMetrics metrics, int availableWidth) {
     if (text == null || availableWidth <= 0) return "";
     trimmed = !isFit(text, metrics, availableWidth);
     if (!trimmed) return text;
-    int ellipsisWidth = metrics.stringWidth(ellipsis);
-    if (availableWidth <= ellipsisWidth) return ellipsis;
-    int width = availableWidth - ellipsisWidth;
-    if (alignment == SwingConstants.LEFT) return ellipsis + trimLeft(text, metrics, width);
-    if (alignment == SwingConstants.RIGHT) return trimRight(text, metrics, width) + ellipsis;
-    String postfix = trimRight(text, metrics, (int)(width * ratio)) + ellipsis;
-    return postfix + trimLeft(text, metrics, availableWidth - metrics.stringWidth(postfix));
+    return strategy.trim(text, metrics, availableWidth);
   }
 
   public boolean isTrimmed() {
@@ -59,37 +45,7 @@ public class SwingTextTrimmer {
     this.trimmed = trimmed;
   }
 
-  private static boolean isFit(@NotNull String text, @NotNull FontMetrics metrics, int width) {
+  static boolean isFit(@NotNull String text, @NotNull FontMetrics metrics, int width) {
     return text.isEmpty() || metrics.stringWidth(text) <= width;
-  }
-
-  private static @NotNull String trimLeft(@NotNull String text, @NotNull FontMetrics metrics, int width) {
-    int min = 0, max = text.length();
-    while (true) {
-      int pos = max - (max - min) / 2;
-      String str = text.substring(pos);
-      if (pos == max) return str;
-      if (isFit(str, metrics, width)) {
-        max = pos;
-      }
-      else {
-        min = pos;
-      }
-    }
-  }
-
-  private static @NotNull String trimRight(@NotNull String text, @NotNull FontMetrics metrics, int width) {
-    int min = 0, max = text.length();
-    while (true) {
-      int pos = min + (max - min) / 2;
-      String str = text.substring(0, pos);
-      if (pos == min) return str;
-      if (isFit(str, metrics, width)) {
-        min = pos;
-      }
-      else {
-        max = pos;
-      }
-    }
   }
 }
