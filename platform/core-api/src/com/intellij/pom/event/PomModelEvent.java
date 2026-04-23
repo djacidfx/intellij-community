1c29cce279c581cd0d7cb84497b4fe41a0066ac9
Max Medvedev <maxim.medvedev@jetbrains.com>
2026-04-23 12:00:08 +0200

    cleanup [psi]: make PomModelEvent final
    GitOrigin-RevId: fb6c942fc2d9e4f921696fbbc329e93eb91b5318

--- platform/core-api/src/com/intellij/pom/event/PomModelEvent.java
+++ platform/core-api/src/com/intellij/pom/event/PomModelEvent.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.pom.event;
 
 import com.intellij.pom.PomModel;
@@ -12,7 +12,7 @@ import java.util.Collections;
 import java.util.EventObject;
 import java.util.Set;
 
-public class PomModelEvent extends EventObject {
+public final class PomModelEvent extends EventObject {
   private PomChangeSet myChangeSet;
 
   @ApiStatus.Internal
