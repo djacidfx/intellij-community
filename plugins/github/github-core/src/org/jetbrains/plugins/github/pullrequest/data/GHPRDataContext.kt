70fd2b73c74d8d25de26235adf2a06d20560872c
Ivan Semenov <ivan.semenov@jetbrains.com>
2026-04-28 18:26:34 +0200

    refactor [github]: drop the PR list polling implementation
    GitOrigin-RevId: 7a70de9f238bb75b95c1c65f9be0daad8b1f3e00

--- plugins/github/github-core/src/org/jetbrains/plugins/github/pullrequest/data/GHPRDataContext.kt
+++ plugins/github/github-core/src/org/jetbrains/plugins/github/pullrequest/data/GHPRDataContext.kt
@@ -1,14 +1,10 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.github.pullrequest.data
 
-import com.intellij.collaboration.async.collectScoped
-import com.intellij.collaboration.async.launchNow
-import com.intellij.collaboration.async.withInitial
 import com.intellij.collaboration.ui.html.AsyncHtmlImageLoader
 import com.intellij.collaboration.ui.icon.IconsProvider
 import com.intellij.collaboration.util.CodeReviewDomainEntity
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.awaitCancellation
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.plugins.github.api.data.GHReactionContent
 import org.jetbrains.plugins.github.api.data.pullrequest.GHPullRequest
@@ -25,7 +21,6 @@ import org.jetbrains.plugins.github.ui.icons.GHAvatarIconsProvider
 class GHPRDataContext internal constructor(
   val scope: CoroutineScope,
   internal val listLoader: GHPRListLoader,
-  internal val listUpdatesChecker: GHPRListUpdatesChecker,
   internal val dataProviderRepository: GHPRDataProviderRepository,
   val securityService: GHPRSecurityService,
   val repositoryDataService: GHPRRepositoryDataService,
@@ -39,18 +34,6 @@ class GHPRDataContext internal constructor(
   internal val interactionState: GHPRPersistentInteractionState,
 ) {
   init {
-    scope.launchNow {
-      listLoader.refreshOrReloadRequests.withInitial(Unit).collectScoped {
-        try {
-          listUpdatesChecker.start()
-          awaitCancellation()
-        }
-        finally {
-          listUpdatesChecker.stop()
-        }
-      }
-    }
-
     dataProviderRepository.addDetailsLoadedListener(scope) { details: GHPullRequest ->
       listLoader.updateData {
         if (it.id == details.id) details else null
