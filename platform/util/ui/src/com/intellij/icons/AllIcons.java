3b32f509a2dc09b984e26b10e448e5a1c927f7cb
Dmitry Yudin <dmitry.yudin@jetbrains.com>
2026-04-28 18:05:59 +0200

    misc [ui]: Update AllIcons.Nodes.McpServer
    GitOrigin-RevId: 7a0d1cb1d5c37da91ca53cb227ab38e819ccf63a

--- platform/util/ui/src/com/intellij/icons/AllIcons.java
+++ platform/util/ui/src/com/intellij/icons/AllIcons.java
@@ -988,7 +988,7 @@ public class AllIcons {
     /** 16x16 */ public static final @NotNull Icon Library = load("expui/nodes/library.svg", 954949276, 2);
     /** 16x16 */ public static final @NotNull Icon Locked = load("expui/nodes/locked.svg", "nodes/locked.svg", 1553434086, 0);
     /** 16x16 */ public static final @NotNull Icon LogFolder = load("expui/nodes/logFolder.svg", "nodes/logFolder.svg", 1356256727, 2);
-    /** 16x16 */ public static final @NotNull Icon McpServer = load("expui/nodes/mcp-server.svg", -1602444318, 2);
+    /** 16x16 */ public static final @NotNull Icon McpServer = load("expui/nodes/mcpServer.svg", -688113088, 2);
     /** 16x16 */ public static final @NotNull Icon Method = load("expui/nodes/method.svg", "nodes/method.svg", 769751382, 1);
     /** 16x16 */ public static final @NotNull Icon MethodReference = load("expui/nodes/methodReference.svg", "nodes/methodReference.svg", -1852540687, 1);
     /** 16x16 */ public static final @NotNull Icon Mnemonic = load("expui/bookmarks/mnemonic.svg", "nodes/mnemonic.svg", 421519755, 2);
